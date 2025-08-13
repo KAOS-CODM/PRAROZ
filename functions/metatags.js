ac41a8bb59f508a4648cab0c6cec729ed03a5fb8
KAOS-CODM <klasikyemite04@gmail.com>
2025-08-13 05:10:33 +0100

    added og meta tags 10

--- functions/metatags.js
+++ functions/metatags.js
@@ -2,71 +2,71 @@ module.exports = {
   index: {
     title: 'Welcome to Praroz™',
     description: 'Praroz™ is a world of delicacies. Your satisfaction, Our joy',
-    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.wep',
+    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.webp',
   },
   home: {
     title: 'Home',
     description: 'Delicious recipes from around the world at Praroz.',
-    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.wep',
+    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.webp',
   },
   desserts: {
     title: 'Desserts',
     description: 'Check out our collection of delicious desserts',
-    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.wep',
+    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.webp',
   },
   appetizers: {
     title: 'Appetizers',
     description: 'Check out our collection of delicious desserts',
-    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.wep',
+    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.webp',
   },
   pasta: {
     title: 'Pasta',
     description: 'Check out our collection of delicious desserts',
-    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.wep',
+    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.webp',
   },
   salad: {
     title: 'Salad',
     description: 'Check out our collection of delicious desserts',
-    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.wep',
+    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.webp',
   },
   burger: {
     title: 'Burger',
     description: 'Check out our collection of delicious desserts',
-    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.wep',
+    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.webp',
   },
   pizza: {
     title: 'Pizza',
     description: 'Check out our collection of delicious desserts',
-    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.wep',
+    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.webp',
   },
   recipes: {
     title: 'More Recipes',
     description: 'Check out our collection of delicious desserts',
-    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.wep',
+    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.webp',
   },
   about: {
     title: 'About Us',
     description: 'Know more about the Praroz™',
-    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.wep',
+    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.webp',
   },
   contact: {
     title: 'Contact Us',
     description: 'Contact us on ay of our provided contact',
-    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.wep',
+    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.webp',
   },
   submission: {
     title: 'Submit Recipes',
     description: 'Submit recipes of your choice and liking',
-    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.wep',
+    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.webp',
   },
   terms: {
     title: 'Terms and Conditions',
     description: 'Our terms and conditions',
-    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.wep',
+    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.webp',
   },
   404: {
     title: '404 | Page not found',
     description: '404 | Page not found',
-    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.wep',
+    image: 'https://praroz.onrender.com/images/thumbnail/praroz_thumbnail.webp',
   }
 };
